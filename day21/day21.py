d634456aacd25611496751a333dfb2622b660683
David Möller <damoller99@hotmail.se>
2024-12-27 14:18:49 +0100

    Finish day21 part 1

--- day21/day21.py
+++ day21/day21.py
@@ -1,5 +1,6 @@
 from typing import Dict, List, Tuple
 from pathlib import Path
+from itertools import permutations
 
 lines = Path("./input.txt").read_text().split("\n")
 
@@ -33,42 +34,69 @@ def arrows_to_coords(arrows: str, arrow_coords=arrow_coords) -> List[Tuple[int, 
     return [arrow_coords["A"]] + [arrow_coords[n] for n in arrows]
 
 
-def coords_to_arrows(coords: List[Tuple[int, int]], num_pad=False) -> str:
-    arrows = ""
+def pass_illegal(pos, path, illegal: List[Tuple[int, int]]) -> bool:
+    if pos in illegal:
+        return True
+    if path == "":
+        return False
+
+    if path[0] == "<":
+        return pass_illegal((pos[0] - 1, pos[1]), path[1:], illegal)
+    if path[0] == ">":
+        return pass_illegal((pos[0] + 1, pos[1]), path[1:], illegal)
+    if path[0] == "v":
+        return pass_illegal((pos[0], pos[1] + 1), path[1:], illegal)
+    if path[0] == "^":
+        return pass_illegal((pos[0], pos[1] - 1), path[1:], illegal)
+
+    raise ValueError(path[0])
+
+
+def get_small_paths(
+    coords: Dict[str, Tuple[int, int]],
+    illegal: List[Tuple[int, int]],
+) -> Dict[Tuple[Tuple[int, int], Tuple[int, int]], List[str]]:
+    small_paths = dict()
+    for c1 in coords.values():
+        for c2 in coords.values():
+            path = ""
+            x_diff = c1[0] - c2[0]
+            y_diff = c1[1] - c2[1]
+            if x_diff > 0:
+                path += "<" * x_diff
+            if x_diff < 0:
+                path += ">" * (-x_diff)
+            if y_diff > 0:
+                path += "^" * y_diff
+            if y_diff < 0:
+                path += "v" * (-y_diff)
+
+            small_paths[(c1, c2)] = [
+                "".join(p)
+                for p in permutations(path)
+                if not pass_illegal(c1, "".join(p), illegal)
+            ]
+
+    return small_paths
+
+
+arrows_small_paths = get_small_paths(arrow_coords, illegal=[(0, 0)])
+num_pad_small_paths = get_small_paths(num_pad_coords, illegal=[(0, 3)])
+
+
+def coords_to_arrows(coords: List[Tuple[int, int]], num_pad=False) -> List[str]:
+    paths = set([""])
+    small_paths = num_pad_small_paths if num_pad else arrows_small_paths
     current = coords.pop(0)
     for c in coords:
-        x_part = ""
-        y_part = ""
-        x_diff = current[0] - c[0]
-        y_diff = current[1] - c[1]
-        if x_diff > 0:
-            x_part += "<" * x_diff
-        if x_diff < 0:
-            x_part += ">" * (-x_diff)
-        if y_diff > 0:
-            y_part += "^" * y_diff
-        if y_diff < 0:
-            y_part += "v" * (-y_diff)
-
-        if current[0] == 0:
-            arrows += x_part + y_part
-        elif num_pad and current[1] == 3:
-            arrows += y_part + x_part
-        elif not num_pad and current[1] == 0:
-            arrows += y_part + x_part
-        else:
-            arrows += x_part + y_part
-
+        new_paths = set()
+        for p in small_paths[(current, c)]:
+            for p2 in paths:
+                new_paths.add(p2 + p + "A")
+        paths = new_paths
         current = c
-        arrows += "A"
-    return arrows
 
-
-def translate1(line: str) -> str:
-    arrows = coords_to_arrows(num_to_coords(line), num_pad=True)
-    for _ in range(2):
-        arrows = coords_to_arrows(arrows_to_coords(arrows))
-    return arrows
+    return list(paths)
 
 
 def arrows_executor(line: str) -> str:
@@ -131,15 +159,23 @@ def num_executor(line: str) -> str:
     return output
 
 
+def translate1(code: str):
+    paths = coords_to_arrows(num_to_coords(code), num_pad=True)
+    for i in range(2):
+        new_paths = set()
+        for path in paths:
+            new_paths.update(coords_to_arrows(arrows_to_coords(path)))
+        min_path = min(*[len(p) for p in new_paths])
+        paths = list([p for p in new_paths if len(p) == min_path])
+        print(len(paths))
+    return paths
+
+
 sum = 0
 for line in lines:
-    orders = translate1(line)
+    min_arrows = min(*[len(p) for p in translate1(line)])
     num = int("".join(filter(lambda x: x in "0123456789", line)))
-    print(num, len(orders))
-    sum += num * len(orders)
+    print(num, min_arrows)
+    sum += num * min_arrows
 
 print(sum)
-
-# for line in lines:
-#     print(line)
-#     assert num_executor(arrows_executor(arrows_executor(translate1(line)))) == line
